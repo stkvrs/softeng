2291eec0f41eee1aee59e2fa6d957ba9f93687c0
Stathis Kavros <stathis.kavros@gmail.com>
2022-05-25 16:56:46 +0300

    initial commit

---
+++ api/users.py
@@ -0,0 +1,110 @@
+import uuid
+from flask_restful import Resource, reqparse
+from flask import jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import mongo
+
+
+class CreateUserAPI(Resource):
+    def __init__(self):
+        self.post_reqparse = reqparse.RequestParser()
+
+        self.post_reqparse.add_argument(
+            "first_name", type=str, required=True, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "last_name", type=str, required=True, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "email", type=str, required=True, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "password", type=str, required=True, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "degree", type=str, required=False, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "grad_date", type=str, required=False, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "description", type=str, required=False, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "hobbies", type=list, required=False, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "imgUrl", type=str, required=False, location="json"
+        )
+
+        super(CreateUserAPI, self).__init__()
+
+    def post(self):
+
+        args = self.post_reqparse.parse_args()
+
+        first_name = args["first_name"]
+        last_name = args["last_name"]
+        email = args["email"]
+        password = args["password"]
+        degree = args["degree"]
+        grad_date = args["grad_date"]
+        description = args["description"]
+        hobbies = args["hobbies"]
+        imgUrl = args["imgUrl"]
+
+        print(args)
+
+        if password is not None:
+            hashed_password = generate_password_hash(password)
+
+        result_dict = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": hashed_password,
+            "degree": degree,
+            "grad_date": grad_date,
+            "description": description,
+            "hobbies": hobbies,
+            "imgUrl": imgUrl,
+        }
+
+        # Check if email already exists
+        if mongo.db.users.find_one({"email": email}):
+            return make_response(jsonify({"Error": "Email is already in use"}), 400)
+
+        mongo.db.users.insert_one(result_dict)
+
+        return make_response(jsonify(result_dict), 201)
+
+
+class LoginUserAPI(Resource):
+    def __init__(self):
+        self.post_reqparse = reqparse.RequestParser()
+
+        self.post_reqparse.add_argument(
+            "email", type=str, required=True, location="json"
+        )
+        self.post_reqparse.add_argument(
+            "password", type=str, required=True, location="json"
+        )
+
+        super(LoginUserAPI, self).__init__()
+
+    def post(self):
+
+        args = self.post_reqparse.parse_args()
+
+        email = args["email"]
+        password = args["password"]
+
+        user = mongo.db.users.find_one({"email": email})
+
+        if user is None:
+            return make_response(jsonify({"Error": "The user doen't exist"}), 400)
+
+        if email != user['email'] or not check_password_hash(user['password'], password):
+            return make_response(jsonify({"Error": "Email and/or password provided are not correct"}), 400)
+
+        return make_response(jsonify(user), 200)
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import json
+from datetime import datetime, date
+import logging
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask.json import JSONEncoder
+from bson.objectid import ObjectId
+from flask_restful import Api
+from extensions import mongo
+from api.users import CreateUserAPI, LoginUserAPI
+
+class CustomJSONEncoder(JSONEncoder):
+    """Custom JSON Encoder which can handle mongo types like ObjectId"""
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        elif isinstance(obj, datetime):
+            return obj.strftime('%d-%m-%Y %H:%M:%S %Z')
+        elif isinstance(obj, date):
+            return obj.strftime('%d-%m-%Y')
+        elif isinstance(obj, json.JSONEncoder):
+            return obj.default(obj)
+        try:
+          return super().default(self, obj)
+        except TypeError as te:
+          logging.warning(f'{te} in CustomJSONEncoder. Object was serialized as obj.__dict__')
+          return obj.__dict__
+
+load_dotenv()
+
+
+app = Flask(__name__)
+api = Api(app)
+
+###### Configuration ##########
+app.json_encoder = CustomJSONEncoder
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+mongo.init_app(app)
+
+
+####### Endpoints ###########
+api.add_resource(CreateUserAPI, "/api/register")
+api.add_resource(LoginUserAPI, "/api/login")
+
+if __name__ == "__main__":
+    app.run(debug=True)
